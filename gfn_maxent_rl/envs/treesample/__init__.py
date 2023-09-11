6134fff9614368dc3aa49b9c54a29101dacfec4c
Tristan Deleu <tristan.deleu@gmail.com>
2023-09-11 15:22:39 -0400

    Add FactorGraphs2 environment from TreeSample

---
+++ gfn_maxent_rl/envs/treesample/__init__.py
@@ -0,0 +1,4 @@
+from gfn_maxent_rl.envs.treesample.chain_env import ChainEnvironment
+from gfn_maxent_rl.envs.treesample.factor_graph1_env import FactorGraph1Environment
+from gfn_maxent_rl.envs.treesample.factor_graph2_env import FactorGraph2Environment
+from gfn_maxent_rl.envs.treesample.permuted_chain_env import PermutedChainEnvironment
