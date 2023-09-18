f85fc38973888727ede6f6b1a54217adfb44ac8a
Tristan Deleu <tristan.deleu@gmail.com>
2023-09-18 10:47:51 -0400

    Update Vanilla SQL and PCL

--- gfn_maxent_rl/algos/path_consistency_learning.py
+++ gfn_maxent_rl/algos/path_consistency_learning.py
@@ -7,22 +7,22 @@ from gfn_maxent_rl.algos.base import GFNBaseAlgorithm
 
 
 class PCL(GFNBaseAlgorithm):
-    def __init__(self, network, update_target_every=0):
+    def __init__(self, env, network, update_target_every=0):
         if update_target_every != 0:
             warnings.warn('No target network used in PCL, but '
                 f'`update_target_every={update_target_every}`. Setting '
                 '`update_target_every=0`.')
-        super().__init__(network, update_target_every=0)
+        super().__init__(env, network, update_target_every=0)
 
     def loss(self, online_params, _, state, samples):
         # Similar to Trajectory Balance loss
 
         # Get log P_F(. | G_t) for full trajectory
-        v_model = jax.vmap(self.network.apply, in_axes=(None, None, 0, 0))
-        log_pi = v_model(online_params.network, state, samples['graphs'], samples['masks'])
+        v_model = jax.vmap(self.network.apply, in_axes=(None, None, 0))
+        log_pi, _ = v_model(online_params.network, state, samples['observations'])
 
         # Mask the log-probabilities, based on the sequence lengths
-        seq_masks = (jnp.arange(log_pi.shape[1]) <= samples['lengths'])
+        seq_masks = (jnp.arange(log_pi.shape[1]) <= samples['lengths'][:, None])
         log_pi = jnp.where(seq_masks[..., None], log_pi, 0.)
 
         # Compute the forward log_probabilities
@@ -30,7 +30,7 @@ class PCL(GFNBaseAlgorithm):
         log_pF = jnp.sum(log_pF, axis=(1, 2))
 
         # Compute the log-rewards, based on the delta-scores
-        log_rewards = jnp.where(seq_masks[..., None], samples['rewards'], 0.)
+        log_rewards = jnp.where(seq_masks, samples['rewards'], 0.)
         log_rewards = jnp.sum(log_rewards, axis=1)
 
         # PCL loss
--- gfn_maxent_rl/algos/soft_qlearning_vanilla.py
+++ gfn_maxent_rl/algos/soft_qlearning_vanilla.py
@@ -7,25 +7,22 @@ from gfn_maxent_rl.algos.base import BaseAlgorithm, AlgoParameters, AlgoState
 
 
 class SoftQLearningVanilla(BaseAlgorithm):
-    def __init__(self, network, update_target_every=0):
-        super().__init__(update_target_every=update_target_every)
+    def __init__(self, env, network, update_target_every=0):
+        super().__init__(env, update_target_every=update_target_every)
         self.network = hk.without_apply_rng(hk.transform_with_state(network))
 
     def loss(self, online_params, target_params, state, samples):
-        batch_size = samples['mask'].shape[0]
-        masks_continue = samples['mask'].reshape(batch_size, -1)
-        mask_stop = jnp.ones((batch_size, 1), dtype=masks_continue.dtype)
-        masks = jnp.concatenate((masks_continue, mask_stop), axis=-1)
+        action_masks = self.env.action_mask(samples['observation'])
 
         # Get Q(G_t, .) for the current graph
         Q_t, _ = self.network.apply(
-            online_params, state, samples['graph'], samples['mask'])
+            online_params, state, samples['observation'])
 
         # Get Q(G_t+1, .) for the next graph
         params = target_params if self.use_target else online_params
         Q_tp1, _ = self.network.apply(
-            params, state, samples['next_graph'], samples['next_mask'])
-        Q_tp1 = jnp.where(masks == 1., Q_tp1, 0.)
+            params, state, samples['next_observation'])
+        Q_tp1 = jnp.where(action_masks, Q_tp1, 0.)
         V_tp1 = Q_tp1.sum(axis=-1)
 
         # Compute the (modified) detailed balance loss
@@ -41,7 +38,7 @@ class SoftQLearningVanilla(BaseAlgorithm):
 
     def init(self, key, samples, normalization=1):
         # Initialize the network parameters (both online, and possibly target)
-        online_params, net_state = self.network.init(key, samples['graph'], samples['mask'])
+        online_params, net_state = self.network.init(key, samples['observation'])
         target_params = online_params if self.use_target else None
         params = AlgoParameters(online=online_params, target=target_params)
 
@@ -55,20 +52,11 @@ class SoftQLearningVanilla(BaseAlgorithm):
         return (params, state)
 
     def log_policy(self, params, state, observations):
-        q_values, _ = self.network.apply(
-            params,
-            state,
-            observations['graph'],
-            observations['mask']
-        )
+        q_values, _ = self.network.apply(params, state, observations)
 
         # Mask invalid actions
-        batch_size = observations['mask'].shape[0]
-        masks_continue = observations['mask'].reshape(batch_size, -1)
-        mask_stop = jnp.ones((batch_size, 1), dtype=masks_continue.dtype)
-        masks = jnp.concatenate((masks_continue, mask_stop), axis=-1)
-
-        logits = jnp.where(masks, q_values, -jnp.inf)
+        action_masks = self.env.action_mask(observations)
+        logits = jnp.where(action_masks, q_values, -jnp.inf)
         log_pi = jax.nn.log_softmax(logits, axis=-1)
 
         return log_pi
