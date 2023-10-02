333bedbe0f3cf75b42a2d4b390ed646a2fc2a4ad
Tristan Deleu <tristan.deleu@gmail.com>
2023-10-02 14:49:20 -0400

    Separate actor and critic losses

--- gfn_maxent_rl/algos/soft_actor_critic.py
+++ gfn_maxent_rl/algos/soft_actor_critic.py
@@ -8,76 +8,87 @@ from collections import namedtuple
 from gfn_maxent_rl.algos.base import BaseAlgorithm, AlgoParameters, AlgoState
 from functools import partial
 
-SACParameters = namedtuple('SACParameters', ['actor', 'critic1', 'critic2'])
+SACParameters = namedtuple('SACParameters', ['actor', 'critic'])
 
 class SAC(BaseAlgorithm):
     def __init__(self, env, actor_network, critic_network, target=None, target_kwargs={}):
         super().__init__(env, target=target, target_kwargs=target_kwargs)
         self.actor_network = hk.without_apply_rng(hk.transform_with_state(actor_network))
         self.critic_network = hk.without_apply_rng(hk.transform_with_state(critic_network))
-        # self.loss_type = loss_type
 
-    def loss(self, online_params, target_params, state, samples, loss_type='critic_loss'):
+    def _apply_critic(self, params, state, observations):
+        Q1, _ = self.critic_network.apply(params[0], state[0], observations)
+        Q2, _ = self.critic_network.apply(params[1], state[1], observations)
+        return (Q1, Q2)
+
+    def actor_loss(self, actor_online_params, critic_online_params, state, samples):
         action_masks_t = self.env.action_mask(samples['observation'])
+
+        # Actor network
+        log_pi_t, _ = self.actor_network.apply(
+            actor_online_params, state.actor, samples['observation'])
+        
+        # Get Q(G_t, .) for the current graph
+        Q1_t, Q2_t = self._apply_critic(critic_online_params, state.critic, samples['observation'])
+
+        min_Q_t = jax.lax.stop_gradient(jnp.minimum(Q1_t, Q2_t))
+        pi_t = jnp.where(action_masks_t, jnp.exp(log_pi_t), 0.)
+        actor_loss = pi_t * (log_pi_t - min_Q_t)
+        actor_loss = jnp.where(action_masks_t, actor_loss, 0.)
+        actor_loss = jnp.sum(actor_loss, axis=-1)
+
+        loss = jnp.mean(actor_loss)
+        logs = {'actor_loss': actor_loss}
+        return (loss, logs)
+
+    def critic_loss(self, critic_online_params, actor_online_params, target_params, state, samples):
         action_masks_tp1 = self.env.action_mask(samples['next_observation'])
 
-        # actor network
-        log_pi_t, _ = self.actor_network.apply(
-            online_params.actor, state.actor, samples['observation'])
+        # Actor network
         log_pi_tp1, _ = self.actor_network.apply(
-            online_params.actor, state.actor, samples['next_observation'])
+            actor_online_params, state.actor, samples['next_observation'])
 
         # Get Q(G_t, .) for the current graph
-        Q1_t, _ = self.critic_network.apply( 
-            online_params.critic1, state.critic1, samples['observation'])
-        Q2_t, _ = self.critic_network.apply(
-            online_params.critic2, state.critic2, samples['observation'])
+        Q1_t, Q2_t = self._apply_critic(critic_online_params, state.critic, samples['observation'])
 
         # Get Q(G_t+1, .) for the next graph
-        params = target_params if self.use_target else online_params
-        Q1_tp1, _ = self.critic_network.apply(
-            params.critic1, state.critic1, samples['next_observation'])
-        Q2_tp1, _ = self.critic_network.apply(
-            params.critic2, state.critic2, samples['next_observation'])
+        params = target_params.critic if self.use_target else critic_online_params
+        Q1_tp1, Q2_tp1 = self._apply_critic(params, state.critic, samples['next_observation'])
 
-
-
-        # critic loss
+        # Critic loss
         log_pi_tp1 = jax.lax.stop_gradient(log_pi_tp1)
         min_Q_tp1 = jnp.exp(log_pi_tp1) * (jnp.minimum(Q1_tp1, Q2_tp1) - log_pi_tp1)
         min_Q_tp1 = jnp.where(action_masks_tp1, min_Q_tp1, 0.)
         V_tp1 = min_Q_tp1.sum(axis=-1)
 
-        # use Q-values only for the taken actions
+        # Use Q-values only for the taken actions
         old_Q1 = jnp.take_along_axis(Q1_t, samples['action'], axis=-1)
         old_Q1 = jnp.squeeze(old_Q1, axis=-1)
         old_Q2 = jnp.take_along_axis(Q2_t, samples['action'], axis=-1)
         old_Q2 = jnp.squeeze(old_Q2, axis=-1)
 
-        # critic loss
         rewards = jnp.squeeze(samples['reward'], axis=-1)
-        if loss_type == 'critic_loss':
-            old_Q1_errors = (rewards + V_tp1 - old_Q1)
-            old_Q1_loss = optax.huber_loss(old_Q1_errors, delta=1.)  # TODO: Modify delta
-            old_Q2_errors = (rewards + V_tp1 - old_Q2)
-            old_Q2_loss = optax.huber_loss(old_Q2_errors, delta=1.)  # TODO: Modify delta
+        old_Q1_errors = (rewards + V_tp1 - old_Q1)
+        old_Q1_loss = optax.huber_loss(old_Q1_errors, delta=1.)  # TODO: Modify delta
+        old_Q2_errors = (rewards + V_tp1 - old_Q2)
+        old_Q2_loss = optax.huber_loss(old_Q2_errors, delta=1.)  # TODO: Modify delta
 
-            critic_loss = old_Q1_loss + old_Q2_loss
-            loss = jnp.mean(critic_loss)
+        critic_loss = old_Q1_loss + old_Q2_loss
+        loss = jnp.mean(critic_loss)
+        logs = {'critic_loss': critic_loss}
+        return (loss, logs)
 
-        # actor loss
-        elif loss_type == 'actor_loss':
-            min_Q_t = jax.lax.stop_gradient(jnp.minimum(Q1_t, Q2_t))
-            pi_t = jnp.where(action_masks_t, jnp.exp(log_pi_t), 0.)
-            actor_loss = pi_t * (log_pi_t - min_Q_t)
-            actor_loss = jnp.where(action_masks_t, actor_loss, 0.)
-            actor_loss = jnp.sum(actor_loss, axis=-1)
-            loss = jnp.mean(actor_loss)
-
-        # loss = jnp.mean(critic_loss + actor_loss)
-
-        # logs = {'loss': loss, 'critic_loss': critic_loss, 'actor_loss': actor_loss}
-        logs = {'loss': loss}
+    def loss(self, online_params, target_params, state, samples):
+        # For compatibility with BaseAlgorithm
+        actor_loss, logs = self.actor_loss(
+            online_params.actor, online_params.critic, state, samples)
+        critic_loss, critic_logs = self.critic_loss(
+            online_params.critic, online_params.actor, target_params, state, samples)
+        loss = actor_loss + critic_loss
+        
+        # Update the logs
+        logs.update(critic_logs)
+        logs['loss'] = loss
         return (loss, logs)
 
     def init(self, key, samples, normalization=1):
@@ -87,7 +98,7 @@ class SAC(BaseAlgorithm):
         actor_params, actor_state = self.actor_network.init(subkey1, samples['observation'])
         critic1_params, critic1_state = self.critic_network.init(subkey2, samples['observation'])
         critic2_params, critic2_state = self.critic_network.init(subkey3, samples['observation'])
-        online_params = SACParameters(actor=actor_params, critic1=critic1_params, critic2=critic2_params)
+        online_params = SACParameters(actor=actor_params, critic=(critic1_params, critic2_params))
 
         target_params = online_params if self.use_target else None
         params = AlgoParameters(online=online_params, target=target_params)
@@ -95,11 +106,17 @@ class SAC(BaseAlgorithm):
         # Set the normalization to the size of the dataset
         actor_state['~']['normalization'] = jnp.full_like(
             actor_state['~']['normalization'], normalization)
-        net_state = SACParameters(actor=actor_state, critic1=critic1_state, critic2=critic2_state)
+        net_state = SACParameters(actor=actor_state, critic=(critic1_state, critic2_state))
+
+        # Initialize the state of the optimizers
+        opt_state = SACParameters(
+            actor=self.optimizer.actor.init(online_params.actor),
+            critic=self.optimizer.critic.init(online_params.critic),
+        )
 
         # Initialize the state
         state = AlgoState(
-            optimizer=self.optimizer.init(online_params),
+            optimizer=opt_state,
             steps=jnp.array(0),
             network=net_state
         )
@@ -112,21 +129,25 @@ class SAC(BaseAlgorithm):
 
     @partial(jax.jit, static_argnums=(0,))
     def step(self, params, state, samples):
-        # Critic update
-        loss_type = 'critic_loss'
-        grads, logs = jax.grad(self.loss, has_aux=True)(params.online, params.target, state.network, samples, loss_type)
+        # Update of the parameters of the critic
+        (critic_loss, logs), grads_critic = jax.value_and_grad(self.critic_loss, has_aux=True)(
+            params.online.critic, params.online.actor, params.target, state.network, samples)
 
-        # Update the online parameters
-        updates, opt_state = self.optimizer.update(grads, state.optimizer, params.online)
-        online_params = optax.apply_updates(params.online, updates)
+        updates_critic, opt_state_critic = self.optimizer.critic.update(
+            grads_critic, state.optimizer.critic, params.online.critic)
+        params_critic = optax.apply_updates(params.online.critic, updates_critic)
 
-        params = AlgoParameters(online=online_params, target=params.target)
+        # Update the parameters of the actor
+        (actor_loss, logs_actor), grads_actor = jax.value_and_grad(self.actor_loss, has_aux=True)(
+            params.online.actor, params_critic, state.network, samples)  # Use the updated critic parameters
 
-        loss_type = 'actor_loss'
-        grads, logs = jax.grad(self.loss, has_aux=True)(params.online, params.target, state.network, samples, loss_type)
-        updates, opt_state = self.optimizer.update(grads, state.optimizer, params.online)
-        # online_params = optax.periodic_update(updates, params.online, state.steps + 1, update_period=1)
-        online_params = optax.apply_updates(params.online, updates)
+        updates_actor, opt_state_actor = self.optimizer.actor.update(
+            grads_actor, state.optimizer.actor, params.online.actor)
+        params_actor = optax.apply_updates(params.online.actor, updates_actor)
+
+        # Pack the parameters & optimizer state
+        online_params = SACParameters(actor=params_actor, critic=params_critic)
+        opt_state = SACParameters(actor=opt_state_actor, critic=opt_state_critic)
 
         # Update the target parameters
         if self.target == 'periodic':
@@ -150,4 +171,7 @@ class SAC(BaseAlgorithm):
         params = AlgoParameters(online=online_params, target=target_params)
         state = AlgoState(optimizer=opt_state, steps=state.steps + 1, network=state.network)
 
+        # Update the logs
+        logs.update(logs_actor)
+        logs['loss'] = actor_loss + critic_loss
         return (params, state, logs)
