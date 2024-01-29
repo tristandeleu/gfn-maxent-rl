7c9b06c3fa31aba5244d8eea79850ed991bc0d4a
Tristan Deleu <tristan.deleu@gmail.com>
2024-01-29 11:46:07 -0500

    Add offset and scale for the reward

--- gfn_maxent_rl/envs/phylo_gfn/factories.py
+++ gfn_maxent_rl/envs/phylo_gfn/factories.py
@@ -7,15 +7,15 @@ from gfn_maxent_rl.envs.phylo_gfn.env import PhyloTreeEnvironment
 
 
 DATA_FOLDER = Path(__file__).resolve().parent / 'datasets'
-SEQUENCE_TYPES = {
-    'DS1': 'DNA_WITH_GAP',
-    'DS2': 'DNA_WITH_GAP',
-    'DS3': 'DNA',
-    'DS4': 'DNA_WITH_GAP',
-    'DS5': 'DNA_WITH_GAP',
-    'DS6': 'DNA_WITH_GAP',
-    'DS7': 'DNA',
-    'DS8': 'DNA_WITH_GAP'
+CONFIGS = {
+    'DS1': ('DNA_WITH_GAP', 5800.0, 4.),
+    'DS2': ('DNA_WITH_GAP', 8000.0, 4.),
+    'DS3': ('DNA_WITH_GAP', 8800.0, 4.),
+    'DS4': ('DNA_WITH_GAP', 3500.0, 4.),
+    'DS5': ('DNA_WITH_GAP', 2300.0, 4.),
+    'DS6': ('DNA_WITH_GAP', 2300.0, 4.),
+    'DS7': ('DNA_WITH_GAP', 12500.0, 4.),
+    'DS8': ('DNA_WITH_GAP', 2800.0, 4.),
 }
 
 def get_phylo_gfn_env(
@@ -24,23 +24,21 @@ def get_phylo_gfn_env(
     data_folder=DATA_FOLDER,
     **kwargs
 ):
-    if dataset_name not in SEQUENCE_TYPES:
+    if dataset_name not in CONFIGS:
         raise ValueError(f'Unknown dataset: {dataset_name}')
 
     with open(data_folder / f'{dataset_name}.json', 'r') as f:
         sequences = json.load(f)
 
-    reward = ExponentialReward(
-        num_nodes=len(sequences),
-        scale=1.,
-        C=0.
-    )
+    sequence_type, C, scale = CONFIGS[dataset_name]
+
+    reward = ExponentialReward(num_nodes=len(sequences), scale=scale, C=C)
 
     env = PhyloTreeEnvironment(
         num_envs=num_envs,
         sequences=sequences,
         reward=reward,
-        sequence_type=SEQUENCE_TYPES[dataset_name]
+        sequence_type=sequence_type
     )
 
-    return (env, {})
+    return (env, CONFIGS[dataset_name])
