d949849b9d083bc8a948e392c23277f286822785
Tristan Deleu <tristan.deleu@gmail.com>
2024-02-04 13:59:10 -0500

    Unify the reward correction wrappers

--- gfn_maxent_rl/envs/dag_gfn/__init__.py
+++ gfn_maxent_rl/envs/dag_gfn/__init__.py
@@ -1,2 +1,1 @@
 from gfn_maxent_rl.envs.dag_gfn.env import DAGEnvironment
-from gfn_maxent_rl.envs.dag_gfn.wrappers import RewardCorrection
--- gfn_maxent_rl/envs/treesample/wrappers.py
+++ gfn_maxent_rl/envs/treesample/wrappers.py
@@ -1,7 +1,6 @@
 import numpy as np
 import gym
 import jax.numpy as jnp
-import math
 
 from gfn_maxent_rl.envs.treesample.factor_graph_env import FactorGraphEnvironment
 
@@ -28,39 +27,3 @@ class FixedOrderingWrapper(gym.Wrapper):
 
     def num_parents(self, observations):
         return jnp.ones((observations.shape[0],), dtype=jnp.int32)
-
-
-class RewardCorrection(gym.Wrapper):
-    def __init__(self, env, alpha=1., weight='all_steps'):
-        super().__init__(env)
-        self.alpha = alpha  # Temperature parameter
-        self.weight = weight
-
-    def step(self, actions):
-        observations, rewards, terminated, truncated, infos = self.env.step(actions)
-
-        if self.weight == 'all_steps':
-            # num_steps = number of steps to the *next observation*
-            num_steps = np.sum(observations['variables'] != -1, axis=1)
-            num_steps = np.maximum(num_steps, 1)  # Avoid log(0)
-            correction = np.where(terminated | truncated, 0., -np.log(num_steps))
-
-        elif self.weight == 'nonzero':
-            if 'active_potentials' not in infos:
-                raise KeyError('Unavaiable key `active_potentials` in `infos` dict.')
-
-            weight = np.mean(infos['active_potentials'], axis=1)
-            correction = np.where(terminated | truncated | (rewards == 0.),
-                0., -math.lgamma(self.env.num_variables + 1) * weight)
-
-        elif self.weight == 'uniform':
-            total_correction = -math.lgamma(self.env.num_variables + 1)
-            correction = np.where(terminated | truncated,
-                0., total_correction / self.env.num_variables)
-
-        else:
-            raise ValueError(f'Unknown weight: {self.weight}')
-
-        rewards = rewards + self.alpha * correction
-        infos['correction'] = correction
-        return (observations, rewards, terminated, truncated, infos)
